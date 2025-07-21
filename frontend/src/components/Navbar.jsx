6b78323129878cd041f70951a5beb2769852d842
Kshitij-Vijay-Pawar <kshitij.vijay.pawar@gmail.com>
2025-07-21 18:54:02 +0530

    feat(frontend): enhance Navbar with search bar and cart icon
    - Add a responsive search bar with transition effects
    - Integrate shopping cart icon with notification badge
    - Update Navbar layout for better alignment and spacing

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { IoMdSearch } from "react-icons/io";
+import { FaCartShopping } from "react-icons/fa6"
 
 
 const MenuLinks = [
@@ -29,7 +30,7 @@ const Navbar = () => {
   return (
     <div className='bg-white dark:bg-gray-900 dark:text-white duration-200 relative z-40'>
         <div className='py-4'>
-            <div className="container">
+            <div className="container flex justify-between items-center">
                 <div className='flex items-center gap-4'>
                     <a href="#" className='text-primary font-semibold tracking-widest text-2xl uppercase sm:text-3xl'>Eshop</a>
                     <div className='hidden lg:block'>
@@ -42,13 +43,18 @@ const Navbar = () => {
                         </ul>
                     </div>
                 </div>
-                <div>
+                <div className='flex justify-between items-center gap-4'>
                     {/* Search Bar Section */}
-                    <div>
+                    <div className='relative group hidden sm:block'>
                         <input type="text" placeholder='Search' className='search-bar'/>
 
-                        <IoMdSearch className='text-xl text-gray-600 dark:text-gray-400 '/>
+                        <IoMdSearch className='text-xl text-gray-600 group-hover:text-primary dark:text-gray-400 absolute top-1/2 -translate-y-1/2 right-3 duration-200'/>
                     </div>
+                    {/* Card section */}
+                    <button className='relative p-3'>
+                        <FaCartShopping className='text-xl text-gray-600 dark:text-gray-400'/>
+                        <div className='w-4 h-4 bg-red-500 text-white rounded-full absolute top-0 right-0 flex items-center justify-center text-xs'>4</div>
+                    </button>
                     {/* Dark Mode Togal */}
                 </div>
             </div>
