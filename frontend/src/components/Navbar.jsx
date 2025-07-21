8cd0626caea3eb6d8a9a3ee25ced8753f09bc5e8
Kshitij-Vijay-Pawar <kshitij.vijay.pawar@gmail.com>
2025-07-22 01:05:23 +0530

    created a hero section

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import { IoMdSearch } from "react-icons/io";
 import { FaCartShopping } from "react-icons/fa6"
+import { FaCaretDown } from "react-icons/fa6";
+import DarkMode from "./DarkMode"
 
 
 const MenuLinks = [
@@ -26,6 +28,12 @@ const MenuLinks = [
     },
 ]
 
+const DropdownLinks = [
+    {
+        
+    }
+]
+
 const Navbar = () => {
   return (
     <div className='bg-white dark:bg-gray-900 dark:text-white duration-200 relative z-40'>
@@ -40,6 +48,13 @@ const Navbar = () => {
                                     <a href={data.link} className='inline-block px-4 font-semibold text-gray-500 hover:text-black dark:hover:text-white duration-200'>{" "}{data.name}</a>
                                 </li>
                             ))}
+                            {/* dropdow */}
+                            <li className='relative cursor-pointer group flex items-center gap-2'>
+                                <a href="#" className='flex items-center gap-[2px] font-semibold text-gray-500 dark:hover:text-white py-2'>Quick Links</a>
+                                <span>
+                                    <FaCaretDown className="group-hover:rotate-180 duration-300"/>
+                                </span>
+                            </li>
                         </ul>
                     </div>
                 </div>
@@ -56,6 +71,9 @@ const Navbar = () => {
                         <div className='w-4 h-4 bg-red-500 text-white rounded-full absolute top-0 right-0 flex items-center justify-center text-xs'>4</div>
                     </button>
                     {/* Dark Mode Togal */}
+                    <div>
+                        <DarkMode />
+                    </div>
                 </div>
             </div>
         </div>
