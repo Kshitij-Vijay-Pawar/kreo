dc005ac6f91b19cb7d9108deffbe8dc575d19a5c
Kshitij-Vijay-Pawar <kshitij.vijay.pawar@gmail.com>
2025-07-26 01:01:07 +0530

    feat(frontend): enhance Registration page with SplashCursor and Aurora background
    - Add SplashCursor component for improved visual effects
    - Integrate Aurora background with customizable color stops and properties
    - Refactor layout for better structure and visual appeal

--- frontend/src/pages/Registration.jsx
+++ frontend/src/pages/Registration.jsx
@@ -2,25 +2,37 @@ import React from 'react'
 import Aurora from '../bites/auro';
 import SpotlightCard from '../bites/card';
 import Form from '../components/Form';
-import SplashCursor from '../bites/cursor'
+import SplashCursor from '../bites/cursor';
+
 
 
 const Registration = () => {
   return (
     <div className='relative w-full h-screen overflow-hidden bg-black'>
-
-      
+      <SplashCursor />
+      <cursor />
+      {/* Aurora as background */}
+      <div className='absolute inset-0 z-0'>
+        <Aurora
+          colorStops={["#3A29FF", "#FF94B4", "#FF3232"]}
+          blend={0.8}
+          amplitude={1.5}
+          speed={0.5}
+        />
+      </div>
       {/* Centered SpotlightCard */}
       <div className='relative z-10 flex items-center justify-center w-full h-full'>
-        <SpotlightCard className="custom-spotlight-card" spotlightColor="rgba(0, 229, 255, 0.2)">
+        <SpotlightCard  className="custom-spotlight-card" spotlightColor="rgba(0, 229, 255, 0.2)">
           <Form />
         </SpotlightCard>
-
+        
       </div>
     </div>
+    
   )
 }
 
 export default Registration
+
 
 
